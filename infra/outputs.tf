273fadd3612297c4df376c7b91949b827ceaee7e
gabriesb <gssgo2006@gmail.com>
2026-07-22 12:43:12 -0300

    fix: fix 5

--- infra/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ecr_repository_uri" {
-  description = "URI do repositório ECR"
-  value       = aws_ecr_repository.agent_ecr.repository_url
-}
--- src/agent.py
+++ src/agent.py
@@ -18,7 +18,7 @@ handler.setFormatter(
         "%Y-%m-%d %H:%M:%S"
     )
 )
-logger.addHandler(handler) #forçar pipeline 4
+logger.addHandler(handler) #forçar pipeline 5
 
 # -----------------------------
 # System Prompt (Geography Expert)
