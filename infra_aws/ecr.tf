67ee48c2c56258cd8ab73f45576a6cd07c39bc31
gabriesb <gssgo2006@gmail.com>
2026-07-22 13:30:54 -0300

    fix: force delete && force destroy for ecr and s3

--- infra_aws/ecr.tf
+++ infra_aws/ecr.tf
@@ -2,6 +2,8 @@ resource "aws_ecr_repository" "agent_ecr" {
   name                 = "agent_ecr"
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
--- infra_aws/s3.tf
+++ infra_aws/s3.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "example" {
   bucket = "agent-runtime-state"
 
+  force_destroy = true 
+
   tags = {
     Name        = "agent-state"
     Environment = "Dev"
