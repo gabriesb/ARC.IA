b6cff961490c2365c6dd833088449cb71cd92652
gabriesb <gssgo2006@gmail.com>
2026-07-22 11:57:47 -0300

    fix: fix3

--- src/agent.py
+++ src/agent.py
@@ -18,7 +18,7 @@ handler.setFormatter(
         "%Y-%m-%d %H:%M:%S"
     )
 )
-logger.addHandler(handler) #forçar pipeline 2
+logger.addHandler(handler) #forçar pipeline 3
 
 # -----------------------------
 # System Prompt (Geography Expert)
