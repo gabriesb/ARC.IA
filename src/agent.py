116b65fb2de4283c5cdf9148163ed1095e3e48e3
gabriesb <gssgo2006@gmail.com>
2026-07-29 09:16:16 -0300

    fix: soccer ai agent

--- src/agent.py
+++ src/agent.py
@@ -18,7 +18,7 @@ handler.setFormatter(
         "%Y-%m-%d %H:%M:%S"
     )
 )
-logger.addHandler(handler) #forçar pipeline 8
+logger.addHandler(handler) #forçar pipeline 9
 
 # -----------------------------
 # System Prompt (Geography Expert)
